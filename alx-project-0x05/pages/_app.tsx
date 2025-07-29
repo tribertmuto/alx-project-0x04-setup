c4c735ae769f7e146ecffd582f39d7c07b20ea04
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-29 11:47:39 +0200

    changes

---
+++ alx-project-0x05/pages/_app.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/layout/Header";
+import type { AppProps } from "next/app";
+import { CountProvider } from "../context/CountContext";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  )
+}
