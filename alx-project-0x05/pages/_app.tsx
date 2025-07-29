f65448c09749f47c6f613c93b39c334e88b6a594
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-29 12:22:37 +0200

    changes

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "@/styles/globals.css";
-import Layout from "../components/layout/Layout";
+import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
-import { CountProvider } from "../context/CountContext";
+import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
